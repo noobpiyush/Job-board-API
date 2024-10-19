b1e0fffe2db2ccb49e333e41becb919bdfb45bc7
Piyush Waghela <piyush.waghela15514@sakec.ac.in>
2024-10-19 23:23:05 +0530

    fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,9 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://job-portal-two-gray.vercel.app/", // No trailing slash
+    origin: "https://job-portal-two-gray.vercel.app", // No trailing slash
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
-
 
 app.get("/health", (req, res) =>{
     res.send("hii there");
